d17d2396f5eaa12bd29d34e9cea958049fe39122
Nitish Kumar Vashisht <71669388+nitish2101@users.noreply.github.com>
2021-11-16 23:38:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+function totalTip(tipPercent, bill) {
+  document.querySelector(".totalTip").value = "$ " + (bill * tipPercent) / 100;
+}
+for (let i = 0; i < document.querySelectorAll(".b").length; i++) {
+  document
+    .querySelectorAll("button")
+    [i].addEventListener("click", function (e) {
+      let tipPercent = this.innerHTML;
+      console.log(e.value);
+      console.log(tipPercent);
+      let bill = document.querySelector(".billAmount").value;
+      let numberOfPerson = document.querySelector(".numberPerson").value;
+      totalTip(tipPercent, bill);
+      tipPerPerson(numberOfPerson, tipPercent, bill);
+    });
+}
+function tipPerPerson(numberOfPerson, tipPercent, bill) {
+  document.querySelector(".tipPerPerson").value =
+    "$ " + (bill * tipPercent) / (100 * numberOfPerson);
+}
+
+function reset() {
+  document.querySelector(".billAmount").value = "";
+  // document.querySelector(".custom").value = "";
+  document.querySelector(".numberPerson").value = "1";
+  document.querySelector(".totalTip").value = "$ 0.00";
+  document.querySelector(".tipPerPerson").value = "$ 0.00";
+}
